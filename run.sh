a1985ede0f3a29c9776581565ace931dcc057a65
Yonathan Koren <yonkornilov@live.com>
2021-03-09 14:37:40 -0500

    Make script more robust (in terms of arguments, exit codes); set output via file such that you can see the execution of the action itself.

--- run.sh
+++ run.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+if [[ -z "$1" ]] || [[ -z "$2" ]]; then
+  echo "Error: two arguments (object prefix, bucket name) required."
+  exit 1
+fi
+
 object_prefix="$1"
 bucket="$2"
 
-aws s3 cp . s3://${bucket}/${object_prefix}/ --recursive --exclude ".gitignore" --exclude ".git/*" 2>&1 > /dev/null
-echo "s3://${bucket}/${object_prefix}"
+aws s3 cp . s3://${bucket}/${object_prefix}/ --recursive --exclude ".gitignore" --exclude ".git/*"
+
+if [[ ! "$?" -eq 0 ]]; then
+  echo "Error: s3 cp command failed. Check that sufficient IAM permissions are supplied."
+  exit 1
+fi
+
+echo "s3://${bucket}/${object_prefix}" > output
