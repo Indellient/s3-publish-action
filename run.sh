95f2d12c38676fd585b4bb4db54450579e30573e
Yonathan Koren <yonkornilov@live.com>
2021-03-10 12:05:56 -0500

    Enable exclusion argument via exclusion GitHub Action input.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o noglob
+
 if [[ -z "$1" ]] || [[ -z "$2" ]] || [[ -z "$3" ]]; then
   echo "Error: three arguments (object prefix, bucket name, clear folder true/false) required. Got: ${@:1}"
   exit 1
@@ -8,6 +10,18 @@ fi
 if [[ "$3" != "true" ]] && [[ "$3" != "false" ]]; then
   echo "Error: third argument (clearing folder true/false) must be one of 'true', 'false'. Got: $3"
   exit 1
+fi
+
+exclusions=""
+if [[ ! -z "$4" ]]; then # optional exclusion argument supplied
+  for i in $(echo $4 | sed "s/,/ /g")
+  do
+    if [[ -z "$exclusions" ]]; then # variable is currently empty
+      exclusions="--exclude ${i}"
+    else
+      exclusions="${exclusions} --exclude ${i}"
+    fi
+  done
 fi
 
 bucket_folder="$1"
@@ -19,8 +33,11 @@ if [[ "$clear_folder" == "true" ]]; then
   aws s3 rm s3://${bucket}/${bucket_folder} --recursive
 fi
 
-echo "Copying files... to s3://${bucket}/${bucket_folder}..."
-aws s3 cp . s3://${bucket}/${bucket_folder}/ --recursive --exclude ".gitignore" --exclude ".git/*"
+echo "Copying files to s3://${bucket}/${bucket_folder}..."
+if [[ ! -z "$exclusions" ]]; then
+  echo "Running with exclusion flags: $exclusions"
+fi
+aws s3 cp . s3://${bucket}/${bucket_folder}/ --recursive ${exclusions}
 
 if [[ ! "$?" -eq 0 ]]; then
   echo "Error: s3 cp command failed. Check that sufficient IAM permissions are supplied."
